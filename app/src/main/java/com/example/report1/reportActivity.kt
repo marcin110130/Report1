1f9e7d4e6602b2fc8a3894a2af0edab3ae1b84c7
marcin1101 <marcinleja5@gmail.com>
2020-05-10 13:06:09 +0200

    initial commit

---
+++ app/src/main/java/com/example/report1/reportActivity.kt
@@ -0,0 +1,95 @@
+package com.example.report1
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.activity_report.*
+import java.io.IOException
+import java.util.*
+
+class reportActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var filePath: Uri? = null;
+    private val PICK_IMAGE_REQUEST =1234
+    internal var storage: FirebaseStorage?=null
+    internal var storageReference: StorageReference?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+
+        //init firebase
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage!!.reference
+
+        //setup button
+
+        btn_choose_image.setOnClickListener(this)
+        btn_upload_image.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if(v === btn_choose_image)
+            showFileChooser()
+        else if(v === btn_upload_image)
+            uploadFile()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_REQUEST &&
+            resultCode == Activity.RESULT_OK &&
+                data != null && data.data !=null)
+        {
+            filePath = data.data;
+            try{
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filePath)
+                image_preview!!.setImageBitmap(bitmap)
+            }catch (e:IOException)
+            {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun uploadFile() {
+        if(filePath !=null)
+        {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+
+            val imageRef = storageReference!!.child("images/"+ UUID.randomUUID().toString())
+            imageRef.putFile(filePath!!)
+                .addOnSuccessListener {
+                    progressDialog.dismiss()
+                    Toast.makeText(applicationContext, "File Uploaded", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener() {
+                    progressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
+                }
+                .addOnProgressListener {taskSnapShot->
+                    val progress = 100.0 *  taskSnapShot.bytesTransferred/taskSnapShot.totalByteCount
+                    progressDialog.setMessage("Uploaded "+progress.toInt() + "%...")
+                }
+
+        }
+
+    }
+
+    private fun showFileChooser() {
+        val intent = Intent()
+        intent.type="image/*"
+        intent.action= Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent,"SELECT PICTURE"),PICK_IMAGE_REQUEST)
+    }
+}
